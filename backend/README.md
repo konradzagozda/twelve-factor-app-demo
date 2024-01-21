caa2a92c83165e1bb9490ea94e3a14b7cba292e9
Konrad Zagozda <konrad.zagozda@grapeup.com>
2024-01-21 17:56:19 +0100

    remove middlewares and apps

---
+++ backend/README.md
@@ -0,0 +1,10 @@
+# Backend
+
+## Run
+
+`poetry shell`
+`python manage.py runserver`
+
+## Docs
+
+<http://127.0.0.1:8000/api/docs>
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,24 +30,9 @@ ALLOWED_HOSTS: list[str] = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-]
+INSTALLED_APPS: list[str] = []
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
+MIDDLEWARE: list[str] = []
 
 ROOT_URLCONF = "config.urls"
 
