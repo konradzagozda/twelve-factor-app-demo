5e5d0f4e282920263845bcacb86542ff30e46002
Konrad Zagozda <konrad.zagozda@grapeup.com>
2024-03-04 21:23:02 +0100

    create scripts to create env files

---
+++ deployment/aws/create-env-file.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# usage: ./create-env-file.sh AWS_PROFILE
+AWS_PROFILE=$1
+# Output .env file
+ENV_FILE="cloud.env"
+
+# Parameters to fetch from SSM
+PARAMS=(
+    "/todo_api/ALLOWED_HOSTS"
+    "/todo_api/DEBUG"
+    "/todo_api/DB_NAME"
+    "/todo_api/DB_USER"
+    "/todo_api/DB_HOST"
+)
+
+# Clear the file content before starting
+: > "$ENV_FILE"
+
+# Fetch each parameter from SSM and append it to the .env file
+for PARAM in "${PARAMS[@]}"; do
+    # Extract the parameter name for the .env file
+    ENV_NAME=$(echo "$PARAM" | awk -F'/' '{print $NF}')
+
+    # Fetch the parameter value from AWS SSM using the specified profile
+    VALUE=$(AWS_PROFILE=$AWS_PROFILE aws ssm get-parameter --name "$PARAM" --query 'Parameter.Value' --output text)
+
+    # Append to the .env file
+    echo "${ENV_NAME}=${VALUE}" >> "$ENV_FILE"
+done
+
+echo "Environment file $ENV_FILE created."
---
+++ deployment/aws/create-env-secret-file.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# usage: ./create-secret-file.sh AWS_PROFILE
+
+AWS_PROFILE=$1
+# Output .env file
+ENV_FILE="cloud.secret.env"
+
+# Parameters to fetch from SSM
+SECRETS=(
+    "/todo_api/DB_PASSWORD"
+)
+
+# Clear the file content before starting
+: > "$ENV_FILE"
+
+# Fetch each parameter from SSM and append it to the .env file
+for SECRET in "${SECRETS[@]}"; do
+    # Extract the parameter name for the .env file
+    ENV_NAME=$(echo "$SECRET" | awk -F'/' '{print $NF}')
+
+    # Fetch the parameter value from AWS SSM using the specified profile
+    VALUE=$(AWS_PROFILE=$AWS_PROFILE aws secretsmanager get-secret-value --secret-id $SECRET --query 'SecretString' --output text)
+
+    # Append to the .env file
+    echo "${ENV_NAME}=${VALUE}" >> "$ENV_FILE"
+done
+
+echo "Environment file $ENV_FILE created."
