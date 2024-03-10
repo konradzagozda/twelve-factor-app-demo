80e6073f8ea5e75ea82c7c685dadf0376099c7b2
Konrad Zagozda <konrad.zagozda@grapeup.com>
2024-03-10 20:33:08 +0100

    fix db security group

--- deployment/aws/2.cluster.tf/rds.tf
+++ deployment/aws/2.cluster.tf/rds.tf
@@ -9,4 +9,25 @@ resource "aws_db_instance" "todo_api_db" {
   password             = data.aws_secretsmanager_secret_version.todo_api_db_password.secret_string
   db_subnet_group_name  = module.vpc.database_subnet_group_name
   skip_final_snapshot = true
+  vpc_security_group_ids = [aws_security_group.todo_api_db_sg.id]
+}
+
+
+resource "aws_security_group" "todo_api_db_sg" {
+  name        = "todo-api-db-sg"
+  description = "Security group for TODO API database"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "todo-api-db-sg"
+  }
+}
+
+resource "aws_security_group_rule" "allow_all_inbound" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1" # all protocols
+  security_group_id = aws_security_group.todo_api_db_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
 }
